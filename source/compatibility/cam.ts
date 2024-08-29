98efd89169688a0164f13507cd9c4e2c4caf1ab4
Jason Hyde <github@2bad.me>
2024-08-29 08:37:14 +0200

    [ts] add return types to compat interface

--- source/compatibility/cam.ts
+++ source/compatibility/cam.ts
@@ -2,39 +2,40 @@
  * Module to provide backward compatibility with versions 0.x
  */
 
-import { EventEmitter } from 'events'
-import { Onvif, OnvifRequestOptions, SetSystemDateAndTimeOptions } from '../onvif.ts'
-import { GetSnapshotUriOptions, GetStreamUriOptions } from '../media.ts'
-import {
+import { EventEmitter } from 'node:events'
+import type { ReferenceToken } from '../interfaces/common.ts'
+import type { SetNTP } from '../interfaces/deviceManagement.ts'
+import type { GetOSDs } from '../interfaces/media.2.ts'
+import type { NetworkHostType } from '../interfaces/onvif.ts'
+import type { GetSnapshotUriOptions, GetStreamUriOptions } from '../media.ts'
+import { Onvif, type OnvifRequestOptions, type SetSystemDateAndTimeOptions } from '../onvif.ts'
+import type {
+  AbsoluteMoveOptions,
+  ContinuousMoveOptions,
   GetPresetsOptions,
   GetStatusOptions,
   GotoHomePositionOptions,
   GotoPresetOptions,
-  AbsoluteMoveOptions,
+  RelativeMoveOptions,
   RemovePresetOptions,
   SetHomePositionOptions,
-  SetPresetOptions,
-  RelativeMoveOptions,
-  ContinuousMoveOptions
+  SetPresetOptions
 } from '../ptz.ts'
-import { SetNTP } from '../interfaces/devicemgmt.ts'
-import { NetworkHostType } from '../interfaces/onvif.ts'
-import { GetOSDs } from '../interfaces/media.2.ts'
-import { ReferenceToken } from '../interfaces/common.ts'
 
-export type Callback = (error: any, result?: any) => void
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type Callback = (error: any, result?: any) => void // eslint-disable-line @typescript-eslint/no-explicit-any
 export type CompatibilityAbsoluteMoveOptions = AbsoluteMoveOptions & { x?: number; y?: number; zoom?: number }
 export type CompatibilityRelativeMoveOptions = RelativeMoveOptions & { x?: number; y?: number; zoom?: number }
-interface CompatibilityContinuousMoveOptions extends ContinuousMoveOptions {
+type CompatibilityContinuousMoveOptions = {
   x?: number
   y?: number
   zoom?: number
   onlySendPanTilt?: boolean
   onlySendZoom?: boolean
-}
+} & ContinuousMoveOptions
 
 export class Cam extends EventEmitter {
-  private onvif: Onvif
+  private readonly onvif: Onvif
   constructor(options: any, callback: Callback) {
     super()
     this.onvif = new Onvif({
@@ -44,7 +45,9 @@ export class Cam extends EventEmitter {
     if (callback) {
       this.onvif
         .connect()
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
   }
@@ -104,250 +107,312 @@ export class Cam extends EventEmitter {
     return Object.fromEntries(Object.values(this.onvif.ptz.presets).map((preset) => [preset.name, preset.token]))
   }
 
-  connect(callback: Callback) {
+  connect(callback: Callback): void {
     this.onvif
       .connect()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  _request(options: OnvifRequestOptions, callback: Callback) {
+  _request(options: OnvifRequestOptions, callback: Callback): void {
     if (typeof callback !== 'function') {
       throw new Error('`callback` must be a function')
     }
     this.onvif
       .request(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getSystemDateAndTime(callback: Callback) {
+  getSystemDateAndTime(callback: Callback): void {
     this.onvif.device
       .getSystemDateAndTime()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  setSystemDateAndTime(value: SetSystemDateAndTimeOptions, callback: Callback) {
+  setSystemDateAndTime(value: SetSystemDateAndTimeOptions, callback: Callback): void {
     this.onvif.device
       .setSystemDateAndTime(value)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getHostname(callback: Callback) {
+  getHostname(callback: Callback): void {
     this.onvif.device
       .getHostname()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getScopes(callback: Callback) {
+  getScopes(callback: Callback): void {
     this.onvif.device
       .getScopes()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  setScopes(value: string[], callback: Callback) {
+  setScopes(value: string[], callback: Callback): void {
     this.onvif.device
       .setScopes(value)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getCapabilities(callback: Callback) {
+  getCapabilities(callback: Callback): void {
     this.onvif.device
       .getCapabilities()
-      .then((result) => callback(null, result.capabilities))
+      .then((result) => {
+        callback(null, result.capabilities)
+      })
       .catch(callback)
   }
 
-  getServiceCapabilities(callback: Callback) {
+  getServiceCapabilities(callback: Callback): void {
     this.onvif.device
       .getServiceCapabilities()
-      .then((result) => callback(null, result.capabilities))
+      .then((result) => {
+        callback(null, result.capabilities)
+      })
       .catch(callback)
   }
 
-  getActiveSources(callback: Callback) {
+  getActiveSources(callback: Callback): void {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     this.onvif
       .getActiveSources()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getVideoSources(callback: Callback) {
+  getVideoSources(callback: Callback): void {
     this.onvif.media
       .getVideoSources()
-      .then((result) => callback(null, result.videoSources))
+      .then((result) => {
+        callback(null, result.videoSources)
+      })
       .catch(callback)
   }
 
-  getVideoSourceConfigurations(callback: Callback) {
+  getVideoSourceConfigurations(callback: Callback): void {
     this.onvif.media
       .getVideoSourceConfigurations()
-      .then((result) => callback(null, result.configurations))
+      .then((result) => {
+        callback(null, result.configurations)
+      })
       .catch(callback)
   }
 
-  getVideoEncoderConfigurations(callback: Callback) {
+  getVideoEncoderConfigurations(callback: Callback): void {
     this.onvif.media
       .getVideoEncoderConfigurations()
-      .then((result) => callback(null, result.configurations))
+      .then((result) => {
+        callback(null, result.configurations)
+      })
       .catch(callback)
   }
 
-  getServices(includeCapability: boolean | Callback, callback: Callback) {
+  getServices(includeCapability: boolean | Callback, callback: Callback): void {
     if (callback) {
       this.onvif.device
         .getServices({ includeCapability: includeCapability as boolean })
-        .then((result) => callback(null, result.service))
+        .then((result) => {
+          callback(null, result.service)
+        })
         .catch(callback)
     } else {
       this.onvif.device
         .getServices()
-        .then((result) => (includeCapability as Callback)(null, result.service))
+        .then((result) => {
+          ;(includeCapability as Callback)(null, result.service)
+        })
         .catch(callback)
     }
   }
 
-  getDeviceInformation(callback: Callback) {
+  getDeviceInformation(callback: Callback): void {
     this.onvif.device
       .getDeviceInformation()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
   getStreamUri(options: GetStreamUriOptions, callback: Callback): void
   getStreamUri(callback: Callback): void
-  getStreamUri(options: GetStreamUriOptions | Callback, callback?: Callback) {
+  getStreamUri(options: GetStreamUriOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.media
         .getStreamUri(options as GetStreamUriOptions)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.media
       .getStreamUri()
-      .then((result) => (options as Callback)(null, result))
+      .then((result) => {
+        ;(options as Callback)(null, result)
+      })
       .catch(options as Callback)
   }
 
   getSnapshotUri(options: GetSnapshotUriOptions, callback: Callback): void
   getSnapshotUri(callback: Callback): void
-  getSnapshotUri(options: GetSnapshotUriOptions | Callback, callback?: Callback) {
+  getSnapshotUri(options: GetSnapshotUriOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.media
         .getSnapshotUri(options as GetSnapshotUriOptions)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.media
       .getSnapshotUri()
-      .then((result) => (options as Callback)(null, result))
+      .then((result) => {
+        ;(options as Callback)(null, result)
+      })
       .catch(options as Callback)
   }
 
-  getNodes(callback: Callback) {
+  getNodes(callback: Callback): void {
     this.onvif.ptz
       .getNodes()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getConfigurations(callback: Callback) {
+  getConfigurations(callback: Callback): void {
     this.onvif.ptz
       .getConfigurations()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getConfigurationOptions(configurationToken: ReferenceToken, callback: Callback) {
+  getConfigurationOptions(configurationToken: ReferenceToken, callback: Callback): void {
     this.onvif.ptz
       .getConfigurationOptions({ configurationToken })
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  systemReboot(callback: Callback) {
+  systemReboot(callback: Callback): void {
     this.onvif.device
       .systemReboot()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
   getPresets(options: GetPresetsOptions, callback: Callback): void
   getPresets(callback: Callback): void
-  getPresets(options: GetPresetsOptions | Callback, callback?: Callback) {
+  getPresets(options: GetPresetsOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.ptz
         .getPresets(options as GetPresetsOptions)
-        .then((result) =>
+        .then((result) => {
           callback(null, Object.fromEntries(Object.values(result).map((preset) => [preset.name, preset.token])))
-        )
+        })
         .catch(callback)
     }
     this.onvif.ptz
       .getPresets()
-      .then((result) =>
-        (options as Callback)(
+      .then((result) => {
+        ;(options as Callback)(
           null,
           Object.fromEntries(Object.values(result).map((preset) => [preset.name, preset.token]))
         )
-      )
+      })
       .catch(options as Callback)
   }
 
-  gotoPreset(options: GotoPresetOptions, callback: Callback) {
+  gotoPreset(options: GotoPresetOptions, callback: Callback): void {
     this.onvif.ptz
       .gotoPreset(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  setPreset(options: SetPresetOptions, callback: Callback) {
+  setPreset(options: SetPresetOptions, callback: Callback): void {
     this.onvif.ptz
       .setPreset(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  removePreset(options: RemovePresetOptions, callback: Callback) {
+  removePreset(options: RemovePresetOptions, callback: Callback): void {
     this.onvif.ptz
       .removePreset(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  gotoHomePosition(options: GotoHomePositionOptions, callback: Callback) {
+  gotoHomePosition(options: GotoHomePositionOptions, callback: Callback): void {
     this.onvif.ptz
       .gotoHomePosition(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  setHomePosition(options: SetHomePositionOptions, callback: Callback) {
+  setHomePosition(options: SetHomePositionOptions, callback: Callback): void {
     this.onvif.ptz
       .setHomePosition(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
   getStatus(options: GetStatusOptions, callback: Callback): void
   getStatus(callback: Callback): void
-  getStatus(options: GetStatusOptions | Callback, callback?: Callback) {
+  getStatus(options: GetStatusOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.ptz
         .getStatus(options as GetStatusOptions)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.ptz
       .getStatus()
-      .then((result) => (options as Callback)(null, result))
+      .then((result) => {
+        ;(options as Callback)(null, result)
+      })
       .catch(options as Callback)
   }
 
@@ -365,7 +430,9 @@ export class Cam extends EventEmitter {
     if (callback) {
       this.onvif.ptz
         .absoluteMove(options)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     } else {
       this.onvif.ptz.absoluteMove(options).catch((err) => this.emit('error', err))
@@ -386,7 +453,9 @@ export class Cam extends EventEmitter {
     if (callback) {
       this.onvif.ptz
         .relativeMove(options)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     } else {
       this.onvif.ptz.relativeMove(options).catch((err) => this.emit('error', err))
@@ -414,7 +483,9 @@ export class Cam extends EventEmitter {
     if (callback) {
       this.onvif.ptz
         .continuousMove(options)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     } else {
       this.onvif.ptz.continuousMove(options).catch((err) => this.emit('error', err))
@@ -424,35 +495,40 @@ export class Cam extends EventEmitter {
   stop(): void
   stop(options: GetStatusOptions, callback: Callback): void
   stop(callback: Callback): void
-  stop(options?: GetStatusOptions | Callback, callback?: Callback) {
+  stop(options?: GetStatusOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.ptz
         .stop(options as GetStatusOptions)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.ptz
       .stop()
       .then((result) => {
         if (typeof options === 'function') {
-          ;(options as Callback)(null, result)
+          options(null, result)
         }
       })
       .catch(options ? (options as Callback) : (error) => this.emit('error', error))
   }
 
-  getNTP(callback: Callback) {
+  getNTP(callback: Callback): void {
     this.onvif.device
       .getNTP()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  setNTP(options: SetNTPOptions, callback: Callback) {
+  setNTP(options: SetNTPOptions, callback: Callback): void {
     if (!Array.isArray(options.NTPManual)) {
       options.NTPManual = []
     }
     // For backward compatibility
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
     if (options.type || options.ipv4Address || options.ipv6Address || options.dnsName || options.extension) {
       // Note the case changes to follow the xml parser rules
       options.NTPManual.push({
@@ -460,75 +536,86 @@ export class Cam extends EventEmitter {
         IPv4Address: options.ipv4Address,
         IPv6Address: options.ipv6Address,
         DNSname: options.dnsName,
+        // @ts-expect-error TODO check this, probably wrong type def
         extension: options.extension
       })
     }
     this.onvif.device
       .setNTP(options)
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getDNS(callback: Callback) {
+  getDNS(callback: Callback): void {
     this.onvif.device
       .getDNS()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getNetworkInterfaces(callback: Callback) {
+  getNetworkInterfaces(callback: Callback): void {
     this.onvif.device
       .getNetworkInterfaces()
-      .then((result) => callback(null, result))
+      .then((result) => {
+        callback(null, result)
+      })
       .catch(callback)
   }
 
-  getOSDs(options?: GetOSDs | Callback, callback?: Callback) {
+  getOSDs(options?: GetOSDs | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.media
         .getOSDs(options as GetOSDs)
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.media
       .getOSDs()
       .then((result) => {
         if (typeof options === 'function') {
-          ;(options as Callback)(null, result)
+          options(null, result)
         }
       })
       .catch(options ? (options as Callback) : (error) => this.emit('error', error))
   }
 
-  getOSDOptions(options?: GetOSDOptions | Callback, callback?: Callback) {
+  getOSDOptions(options?: GetOSDOptions | Callback, callback?: Callback): void {
     if (callback) {
       this.onvif.media
         .getOSDOptions({
           configurationToken: (options as GetOSDOptions).videoSourceConfigurationToken
         })
-        .then((result) => callback(null, result))
+        .then((result) => {
+          callback(null, result)
+        })
         .catch(callback)
     }
     this.onvif.media
       .getOSDOptions()
       .then((result) => {
         if (typeof options === 'function') {
-          ;(options as Callback)(null, result)
+          options(null, result)
         }
       })
       .catch(options ? (options as Callback) : (error) => this.emit('error', error))
   }
 }
 
-interface GetOSDOptions {
+type GetOSDOptions = {
   videoSourceConfigurationToken: string
 }
 
-interface SetNTPOptions extends SetNTP {
+type SetNTPOptions = {
   // For backward compatibility
   type?: NetworkHostType
   ipv4Address?: string
   ipv6Address?: string
   dnsName?: string
   extension?: string
-}
+} & SetNTP
